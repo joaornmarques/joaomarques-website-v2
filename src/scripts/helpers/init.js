c58b5aa9fdf018846aefb4a139349ea785947a16
joaornmarques <joaornmarques@gmail.com>
2017-09-20 17:39:41 +0100

    rotating cube structure

--- src/scripts/helpers/init.js
+++ src/scripts/helpers/init.js
@@ -6,9 +6,33 @@ $('.title-main-js').show();
 
 setTimeout(function(){
 	$('.title-second-js').show();
-}, 300);
+}, 200);
 
 setTimeout(function(){
 	$('.text-landing-js').show();
 	$('.c-image--landing').show();
-}, 800);
+}, 400);
+
+
+function cssParallax(cont, el, radiusVal){
+  $(cont).mousemove(function(event) {
+
+      cx = Math.ceil($(window).width() / 2.0);
+      cy = Math.ceil($(window).height() / 2.0);
+      dx = event.pageX - cx;
+      dy = event.pageY - cy;
+
+      tiltx = (dy / cy);
+      tilty = - (dx / cx);
+      radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+      degree = (radius * radiusVal);
+
+      $(el, cont).css('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+      $(el, cont).css('transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+  });
+}
+
+$(document).ready(function() {
+  cssParallax('.parallax-container', '.parallax-base', 70);
+  cssParallax('.parallax-container.first', '.parallax-base', 70);
+});
