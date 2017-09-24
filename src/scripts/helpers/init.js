16ea114e09d37ab646b321d30da19c5f78b7b836
joaornmarques <joaornmarques@gmail.com>
2017-09-24 16:28:56 +0100

    image and sounds directory fixed

--- src/scripts/helpers/init.js
+++ src/scripts/helpers/init.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
 
 	var hoverSound = document.createElement('audio');
 	var clickSound = document.createElement('audio');
-  hoverSound.setAttribute('src', '../assets/sounds/hover.wav');
-  clickSound.setAttribute('src', '../assets/sounds/click.wav');
+  hoverSound.setAttribute('src', 'assets/sounds/hover.wav');
+  clickSound.setAttribute('src', 'assets/sounds/click.wav');
   hoverSound.volume = 0.3;
   clickSound.volume = 0.2;
 
