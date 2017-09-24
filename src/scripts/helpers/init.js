fc4201d9a996c63b7364c066ab56a833cc0e08a5
joaornmarques <joaornmarques@gmail.com>
2017-09-24 20:08:01 +0100

    Page transitions

--- src/scripts/helpers/init.js
+++ src/scripts/helpers/init.js
@@ -1,5 +1,5 @@
 /*-----------------------------
----------Landing load----------
+-----------Page load-----------
 -----------------------------*/
 $(function() {
 	$('.body-js').hide();
@@ -7,16 +7,21 @@ $(function() {
 		$('.body-js').show();
 		$('.o-container').addClass('o-container--intro-animate');
 		$('.o-nav').addClass('o-nav--intro-animate');
+		setTimeout(function() {
+			$('.c-cube-container').addClass('c-cube-container--intro-animate');
+		}, 500);
 	});
 });
+
+
 
 /*-----------------------------
 -------------Sound-------------
 -----------------------------*/
-$(document).ready(function() {
+var hoverSound = document.createElement('audio');
+var clickSound = document.createElement('audio');
 
-	var hoverSound = document.createElement('audio');
-	var clickSound = document.createElement('audio');
+$(document).ready(function() {
   hoverSound.setAttribute('src', 'assets/sounds/hover.wav');
   clickSound.setAttribute('src', 'assets/sounds/click.wav');
   hoverSound.volume = 0.3;
@@ -26,9 +31,43 @@ $(document).ready(function() {
   	hoverSound.play();
   });
 
-  $('.click-sound-js').click(function(){
+  $('a, .click-sound-js').click(function(){
   	hoverSound.pause();
   	clickSound.play();
   });
 
 });
+
+
+
+/*-----------------------------
+-------Click animation---------
+-----------------------------*/
+
+$(function() {
+	$('a').click(function(evt) {
+
+		if (!$(this).hasClass("external-click-js")) {
+
+			evt.preventDefault();
+			var link = $(this).attr("href");
+
+			$('.o-container').removeClass('o-container--intro-animate');
+			$('.o-nav').removeClass('o-nav--intro-animate');
+			$('.c-cube-container').removeClass('c-cube-container--intro-animate');
+			hoverSound.volume = 0;
+
+	    setTimeout(function() {
+	      window.location.href = link;
+	    }, 1200);
+
+	  }
+
+	});
+});
+
+
+
+
+
+
